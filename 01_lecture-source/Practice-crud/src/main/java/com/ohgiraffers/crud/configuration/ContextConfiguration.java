8ea952b4da59ecee4df958b07344915ddbbb3c8b
tesla1220 <iwbyl1220@gmail.com>
2024-06-04 16:41:43 +0900

    jpa

--- 01_lecture-source/Practice-crud/src/main/java/com/ohgiraffers/crud/configuration/ContextConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.crud.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.ohgiraffers.crud")
-public class ContextConfiguration {
-}
--- 01_lecture-source/Review_exception-handler/src/main/java/com/ohgiraffers/exceptionhandler/MainController.java
+++
@@ -1,9 +0,0 @@
-package com.ohgiraffers.exceptionhandler;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class MainController {
-
-
-}
