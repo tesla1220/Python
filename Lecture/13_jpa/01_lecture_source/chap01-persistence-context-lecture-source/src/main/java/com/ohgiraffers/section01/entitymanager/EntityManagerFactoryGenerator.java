164c1b753e6517881e28526bb03aabbade91d8f1
tesla1220 <iwbyl1220@gmail.com>
2024-06-05 12:07:23 +0900

    update00

--- Lecture/13_jpa/01_lecture_source/chap01-persistence-context-lecture-source/src/main/java/com/ohgiraffers/section01/entitymanager/EntityManagerFactoryGenerator.java
+++ Lecture/13_jpa/01_lecture_source/chap01-persistence-context-lecture-source/src/main/java/com/ohgiraffers/section01/entitymanager/EntityManagerFactoryGenerator.java
@@ -7,7 +7,14 @@ public class EntityManagerFactoryGenerator {
 
     // singleton 으로 만들기
     private static EntityManagerFactory factory
+                /* Singleton 클래스의 유일한 인스턴스를 저장할 instance라는 이름의 변수를 선언.
+                단지 EntityManagerFactory 타입의 인스턴스 factory 를 저장할 준비가 된 상태야. 실제로 인스턴스를 생성하려면 생성자를 호출해야 해 */
+
             = Persistence.createEntityManagerFactory("jpatest");
+            /*  Persistence.createEntityManagerFactory("jpatest")는 JPA에서 제공하는 정적 메서드로,
+                주어진 이름("jpatest")을 사용하여 EntityManagerFactory를 생성합니다.
+                "jpatest"는 persistence.xml 파일에 정의된 영속성 유닛(Persistence Unit) 이름입니다. */
+
 
     /*
     EntityManagerFactory는 JPA(Java Persistence API)의 중요한 구성 요소로,
@@ -16,10 +23,6 @@ public class EntityManagerFactoryGenerator {
         이는 EntityManagerFactory 타입의 factory 변수가 클래스 로드 시점에 한 번만 생성된다는 것을 의미합니다.
         private로 선언되어 있어 외부에서 직접 접근할 수 없습니다.
         이는 외부에서 인스턴스를 생성하거나 수정하지 못하도록 막기 위함입니다.
-
-     * Persistence.createEntityManagerFactory("jpatest")는 JPA에서 제공하는 정적 메서드로,
-     * 주어진 이름("jpatest")을 사용하여 EntityManagerFactory를 생성합니다.
-     * "jpatest"는 persistence.xml 파일에 정의된 영속성 유닛(Persistence Unit) 이름입니다.
 
          **퍼시스턴스(Persistence)**는 "지속성"이라고 할 수 있어.
          쉽게 말하면, 우리가 컴퓨터에 어떤 정보를 저장하고, 그 정보를 나중에 다시 사용할 수 있게 하는 것을 말해.
@@ -30,11 +33,12 @@ public class EntityManagerFactoryGenerator {
 
     // 이 클래스 외부에서 접근 못하도록 아래와 같이 설정
     private EntityManagerFactoryGenerator() {}
-
-    /* 생성자를 private 으로 선언하여 외부에서 이 클래스를 인스턴스화할 수 없게 합니다.
-    이는 싱글톤 패턴의 중요한 부분으로, 클래스의 인스턴스가 오직 하나만 존재하도록 보장합니다.
-    * */
-
+    /*
+    EntityManagerFactoryGenerator 클래스의 생성자를 정의한 거야.
+    private 으로 선언되어 있기 때문에 외부에서 이 생성자를 직접 호출할 수 없어.
+    즉, 외부에서 new EntityManagerFactoryGenerator()으로 새로운 인스턴스를 만들 수 없도록 막는 거야.
+    이렇게 하면 EntityManagerFactoryGenerator 클래스 내부에서만 인스턴스를 생성할 수 있어.
+    */
 
     // 상기 메소드들은 다 private 로 외부에서 접근 불가하기 때문에 외부 접근 가능하도록 아래 메소드 만들어줌
     public static EntityManagerFactory getInstance() {
--- Practice/JPA/pr_/src/main/java/com/ohgiraffers/section01/entitymanager/EntityManagerFactoryGenerator.java
+++ Practice/JPA/pr_/src/main/java/com/ohgiraffers/section01/entitymanager/EntityManagerFactoryGenerator.java
@@ -7,7 +7,9 @@ import jakarta.persistence.Persistence;
 public class EntityManagerFactoryGenerator {
 
     private static EntityManagerFactory factory
+            // 인스턴스를 저장할 변수를 선언. 아직 인스턴스 생성하지 않음
             = Persistence.createEntityManagerFactory("jpatest");
+            //
 
     private EntityManagerFactoryGenerator() {}
 
